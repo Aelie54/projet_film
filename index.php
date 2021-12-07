ac78213675cb2bbc63a48606d3ceaf7eeafee4fc
Aelie54 <klein.amelie1991@gmail.com>
2021-12-07 11:20:14 +0100

    debut

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php session_start();
+//session_destroy();
+?>
+
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="monblog" content="articles blog">
+  <meta name="keywords" content="blog">
+  <link rel="stylesheet" href="./asset/style2.css">
+  <script src="script.js"></script> 
+  <title>Blog Poésie</title>
+</head>
+<body>
+
+    <header><h1>Le blog Poésie d'Amélie</h1></header>
+    <!--  
+            <nav id="navigation2">
+    <a href="default.html">Home</a>
+    <a href="news.asp">News</a>
+    <a href="about.asp">About</a>
+    <a href="connexion.html">Se connecter</a> 
+            </nav>
+        "-->
+
+<main id="main" >  
